b25287189fa7fe5fa1bc6ffb050a7e67731ab3d0
Tal Zaken <talzaken@gmail.com>
2022-01-21 16:56:53 -0500

    FormHelper on all forms

--- beantowncompost/dropoff_locations/forms.py
+++ beantowncompost/dropoff_locations/forms.py
@@ -8,24 +8,37 @@ from crispy_forms.bootstrap import FormActions
 class DropoffLocationForm(ModelForm):
     class Meta:
         model = DropoffLocation
-        fields = ['neighborhood_name','location_name','location_description','location_address','phone','url','longitude','latitude', 'city']
-
+        fields = ['location_name','location_description','location_address', 'neighborhood_name', 'city', 'phone','website','longitude','latitude']
+    
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.form_method = 'POST'
+            
+            self.helper.layout = Layout(
+                Div(Column('location_name', css_class='col-4'), Column('location_address', css_class='col-8'), css_class='row'),
+                'location_description',
+                Div(Column('neighborhood_name', css_class='col-6'), Column('city', css_class='col-6'), css_class='row'),
+                Div(Column('phone', css_class='col-4'), Column('website', css_class='col-8'), css_class='row'),
+                'longitude',
+                'latitude',
+                FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3'))
+            )
 class AddDropoffLocationForm(ModelForm):
     class Meta:
         model = AddDropoffLocation
-        fields = ['location_name','location_description','location_address', 'neighborhood_name', 'city', 'phone','url','longitude','latitude']
+        fields = ['location_name','location_description','location_address', 'neighborhood_name', 'city', 'phone','website','longitude','latitude']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'POST'
-        #self.helper.add_input()
 
         self.helper.layout = Layout(
-            Div(Column('location_name', css_class='col-3'), Column('location_address', css_class='col-9'), css_class='row'),
+            Div(Column('location_name', css_class='col-4'), Column('location_address', css_class='col-8'), css_class='row'),
             'location_description',
             Div(Column('neighborhood_name', css_class='col-6'), Column('city', css_class='col-6'), css_class='row'),
-            Div(Column('phone', css_class='col-3'), Column('url', css_class='col-9'), css_class='row'),
+            Div(Column('phone', css_class='col-4'), Column('website', css_class='col-8'), css_class='row'),
             'longitude',
             'latitude',
             FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3', onclick='validate_latlng()'))
@@ -36,7 +49,22 @@ class AddDropoffLocationForm(ModelForm):
 class CorrectDropoffLocationForm(ModelForm):
     class Meta:
         model = CorrectDropoffLocation
-        fields = ['neighborhood_name','location_name','location_description','location_address','phone','url','longitude','latitude', 'city']
+        fields = ['location_name','location_description','location_address', 'neighborhood_name', 'city', 'phone','website','longitude','latitude']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        
+        self.helper.layout = Layout(
+            Div(Column('location_name', css_class='col-4'), Column('location_address', css_class='col-8'), css_class='row'),
+            'location_description',
+            Div(Column('neighborhood_name', css_class='col-6'), Column('city', css_class='col-6'), css_class='row'),
+            Div(Column('phone', css_class='col-4'), Column('website', css_class='col-8'), css_class='row'),
+            'longitude',
+            'latitude',
+            FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3'))
+        )
 
 
 class VoteDropoffLocationForm(ModelForm):
@@ -48,7 +76,6 @@ class VoteDropoffLocationForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'POST'
-        #self.helper.add_input()
 
         self.helper.layout = Layout(
             Div(Column('first_name', css_class='col-6'), Column('last_name', css_class='col-6'), css_class='row'),
