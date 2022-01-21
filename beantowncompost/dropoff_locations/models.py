420fb59ce0c96507a7cfd88ab5caaea2faf807aa
Tal Zaken <talzaken@gmail.com>
2022-01-20 22:54:55 -0500

    Made models for Adding, Correcting, Voting

--- beantowncompost/dropoff_locations/models.py
+++ beantowncompost/dropoff_locations/models.py
@@ -12,10 +12,62 @@ class DropoffLocation(models.Model):
     x = models.FloatField(default=None)
     y = models.FloatField(default=None)
     # GeoDjango-specific: a geometry field (MultiPolygonField)
-    point = models.PointField(srid=4326, default=None)
-    city = models.CharField(max_length=100, default=None)
-
+    point = models.PointField(srid=4326, default=None, null=True)
+    city = models.CharField(max_length=100, default=None, null=True)
 
     # Returns the string representation of the model.
     def __str__(self):
-        return self.neighborhood_name
+        return self.location_name
+
+class AddDropoffLocation(models.Model):
+    # Regular Django fields corresponding to the attributes in dropoff_locations.csv
+    neighborhood_name = models.CharField(max_length=100)
+    location_name = models.CharField(max_length=100)
+    location_description = models.CharField(max_length=1000)
+    location_address = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=500)
+    url = models.CharField(max_length=500)
+    x = models.FloatField(default=None)
+    y = models.FloatField(default=None)
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    point = models.PointField(srid=4326, default=None, null=True)
+    city = models.CharField(max_length=100, default=None, null=True)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.location_name
+
+
+class CorrectDropoffLocation(models.Model):
+    # Regular Django fields corresponding to the attributes in dropoff_locations.csv
+    neighborhood_name = models.CharField(max_length=100)
+    location_name = models.CharField(max_length=100)
+    location_description = models.CharField(max_length=1000)
+    location_address = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=500)
+    url = models.CharField(max_length=500)
+    x = models.FloatField(default=None)
+    y = models.FloatField(default=None)
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    point = models.PointField(srid=4326, default=None, null=True)
+    city = models.CharField(max_length=100, default=None, null=True)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.location_name
+
+
+class VoteDropoffLocation(models.Model):
+    # Regular Django fields corresponding to the attributes in dropoff_locations.csv
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=1000)
+    zip = models.CharField(max_length=1000)
+    x = models.FloatField(default=None)
+    y = models.FloatField(default=None)
+    # GeoDjango-specific: a geometry field (MultiPolygonField)
+    point = models.PointField(srid=4326, default=None, null=True)
+
+    # Returns the string representation of the model.
+    def __str__(self):
+        return self.first_name + " " + self.last_name
