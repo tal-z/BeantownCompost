d8b57a4907ad8b9825b77849d8de0f9deafc22b6
Tal Zaken <talzaken@gmail.com>
2022-01-20 22:55:46 -0500

    Made urls for Adding, Correcting, Voting

--- beantowncompost/dropoff_locations/urls.py
+++ beantowncompost/dropoff_locations/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('vote/', views.vote),
+    path('add_location/', views.add_location),
+    path('correct_location/', views.correct_location),
 ]
