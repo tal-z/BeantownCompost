d5addd3d975a3ec9e6936f011e899b7c821c9866
Tal Zaken <talzaken@gmail.com>
2022-01-21 14:37:28 -0500

    render maps in more templates.

--- beantowncompost/dropoff_locations/views.py
+++ beantowncompost/dropoff_locations/views.py
@@ -11,29 +11,29 @@ from .forms import DropoffLocationForm, AddDropoffLocationForm, CorrectDropoffLo
 from django.forms import HiddenInput
 from django.contrib import messages
 
-def get_map():
+def get_map(height='100%'):
     start_coords = (42.36034, -71.0578633)
-    folium_map = folium.Map(location=start_coords, zoom_start=12, tiles='OpenStreetMap')
+    folium_map = folium.Map(location=start_coords, zoom_start=12, tiles='OpenStreetMap', height=height)
     plugins.LocateControl(keepCurrentZoomLevel=True).add_to(folium_map)
     locations = DropoffLocation.objects.all()
     for dropoff in locations:
-            iframe = "<br>".join([f"<b>{dropoff.neighborhood_name}</b>",
-                                  dropoff.location_name + "<br>",
-                                  f"<b>Location Instructions:</b>  {dropoff.location_description}",
-                                  f"<b>Address:</b>  {dropoff.location_address}",
-                                  f"<b>City:</b>  {dropoff.city}",
-                                  f"<b>Phone:</b>  {dropoff.phone}<br>",
-                                  f"<b><a target ='_blank' href='{dropoff.url}'>Visit Website</b></a>",
-                                  f"<a target ='_parent' href='/correct_location/?id={dropoff.id}'><b>Submit a correction for this bin</b></a>"
-                                  ]
-                                 )
-            popup = folium.Popup(iframe,
-                                 min_width=250,
-                                 max_width=500)
-            folium.Marker([dropoff.y, dropoff.x],
-                          popup=popup,
-                          icon=folium.Icon(color="green", icon="fa-trash", prefix='fa'),
-                          ).add_to(folium_map)
+        iframe = "<br>".join([f"<b>{dropoff.neighborhood_name}</b>",
+                                dropoff.location_name + "<br>",
+                                f"<b>Location Instructions:</b>  {dropoff.location_description}",
+                                f"<b>Address:</b>  {dropoff.location_address}",
+                                f"<b>City:</b>  {dropoff.city}",
+                                f"<b>Phone:</b>  {dropoff.phone}<br>",
+                                f"<b><a target ='_blank' href='{dropoff.url}'>Visit Website</b></a>",
+                                f"<a target ='_parent' href='/correct_location/?id={dropoff.id}'><b>Submit a correction for this bin</b></a>"
+                                ]
+                                )
+        popup = folium.Popup(iframe,
+                                min_width=250,
+                                max_width=500)
+        folium.Marker([dropoff.latitude, dropoff.longitude],
+                        popup=popup,
+                        icon=folium.Icon(color="green", icon="fa-trash", prefix='fa'),
+                        ).add_to(folium_map)
     return folium_map
 
 
@@ -44,7 +44,6 @@ def index(request):
 
 
 def vote(request):
-    print("voting!")
     if request.method == 'POST':
         form = VoteDropoffLocationForm(request.POST)
         if form.is_valid():
@@ -63,8 +62,11 @@ def add_location(request):
         if form.is_valid():
             form.save()
             return render(request, 'dropoff_locations/thanks.html', {'action': 'submitting a new location'})
+    map = get_map()
+    map_html = map._repr_html_()
+    map_id = map.get_name()
     form = AddDropoffLocationForm()
-    return render(request, 'dropoff_locations/add_location.html', {'form': form})
+    return render(request, 'dropoff_locations/add_location.html', {'map': map_html, 'map_id': map_id, 'form': form})
 
 
 def correct_location(request):
@@ -73,8 +75,28 @@ def correct_location(request):
         if form.is_valid():
             form.save()
             return render(request, 'dropoff_locations/thanks.html', {'action': 'submitting your correction'})
-    location = DropoffLocation.objects.get(pk=request.GET.get('id', None))
-    form = DropoffLocationForm(instance=location)
-    return render(request, 'dropoff_locations/correct_location.html', {'form': form})
+    dropoff = DropoffLocation.objects.get(pk=request.GET.get('id', None))
+    form = DropoffLocationForm(instance=dropoff)
+    map = folium.Map(location=[dropoff.latitude, dropoff.longitude], zoom_start=12, tiles='OpenStreetMap')
+    iframe = "<br>".join([f"<b>{dropoff.neighborhood_name}</b>",
+                                dropoff.location_name + "<br>",
+                                f"<b>Location Instructions:</b>  {dropoff.location_description}",
+                                f"<b>Address:</b>  {dropoff.location_address}",
+                                f"<b>City:</b>  {dropoff.city}",
+                                f"<b>Phone:</b>  {dropoff.phone}<br>",
+                                f"<b><a target ='_blank' href='{dropoff.url}'>Visit Website</b></a>",
+                                f"<a target ='_parent' href='/correct_location/?id={dropoff.id}'><b>Submit a correction for this bin</b></a>"
+                                ]
+                                )
+    popup = folium.Popup(iframe,
+                            min_width=250,
+                            max_width=500)
+    folium.Marker([dropoff.latitude, dropoff.longitude],
+                    popup=popup,
+                    icon=folium.Icon(color="green", icon="fa-trash", prefix='fa'),
+                    ).add_to(map)
+    map_html = map._repr_html_()
+    map_id = map.get_name()
+    return render(request, 'dropoff_locations/correct_location.html', {'map': map_html, 'map_id': map_id, 'form': form})
 
     
