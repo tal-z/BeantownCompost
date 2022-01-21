1a776b2c36d5c0bb4eeaf626eca9e9f643eeae5e
Tal Zaken <talzaken@gmail.com>
2022-01-21 16:57:44 -0500

    url for list of locations

--- beantowncompost/dropoff_locations/urls.py
+++ beantowncompost/dropoff_locations/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('vote/', views.vote),
     path('add_location/', views.add_location),
     path('correct_location/', views.correct_location),
+    path('locations/', views.locations),
 ]
