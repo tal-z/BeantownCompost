77e81eafe5b7928abc9d6aa8f2b4919baddb0030
Tal Zaken <talzaken@gmail.com>
2022-01-25 16:39:48 -0500

    added auth routes with custom auth form to urls.py

--- beantowncompost/beantowncompost/urls.py
+++ beantowncompost/beantowncompost/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from managers import views as manager_views
+from managers.forms import LoginForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', manager_views.register, name='register'),
+    path('profile/', manager_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='managers/login.html',
+            authentication_form=LoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='managers/logout.html'), name='logout'),
     path('', include('dropoff_locations.urls')),
     path('dropoff/', include('dropoff_locations.urls')),
 ]
