38981cf063ff117bf70f411ad9f480039b3c2a5d
Tal Zaken <talzaken@gmail.com>
2022-01-25 16:40:04 -0500

    added name to home route

--- beantowncompost/dropoff_locations/urls.py
+++ beantowncompost/dropoff_locations/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('vote/', views.vote),
     path('add_location/', views.add_location),
     path('correct_location/', views.correct_location),
