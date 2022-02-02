02afd8270980e8520c1cb057d2b90665140dd9e6
Tal Zaken <talzaken@gmail.com>
2022-02-02 14:43:11 -0500

    custom forms with crispy_form for user registration and account reset

--- beantowncompost/managers/forms.py
+++ beantowncompost/managers/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm, PasswordChangeForm, SetPasswordForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Column, Submit
 from crispy_forms.bootstrap import FormActions
@@ -38,5 +38,51 @@ class LoginForm(AuthenticationForm):
         self.helper.layout = Layout(
             Div('username', placeholder="Username",  css_class="row mt-2"),
             Div('password', placeholder="Password",  css_class="row mt-2"),
+            FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3 mb-3'))
+        )
+
+
+class PasswordChangeForm(PasswordChangeForm):
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordChangeForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'reset-form'
+
+        #self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            Div('old_password', placeholder="Old Password",  css_class="row mt-2"),
+            Div('new_password1', placeholder="New Password",  css_class="row mt-2"),
+            Div('new_password2', placeholder="Confirm New Password",  css_class="row mt-2"),
+            FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3 mb-3'))
+        )
+
+class PasswordResetForm(PasswordResetForm):
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'reset-form'
+
+        #self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            Div('email', placeholder="email address",  css_class="row mt-2"),
+            FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3 mb-3'))
+        )
+
+class SetPasswordForm(SetPasswordForm):
+
+    def __init__(self, *args, **kwargs):
+        super(SetPasswordForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_class = 'reset-form'
+
+        #self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            Div('new_password1', placeholder="New Password",  css_class="row mt-2"),
+            Div('new_password2', placeholder="Confirm New Password",  css_class="row mt-2"),
             FormActions(Submit('Submit', 'Submit', css_class='btn btn-light mt-3 mb-3'))
         )
