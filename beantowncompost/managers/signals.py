596f69945ae2b4753774914838be0476f1acd3a4
Tal Zaken <talzaken@gmail.com>
2022-02-07 22:32:43 -0500

    removed create profile signals...

--- beantowncompost/managers/signals.py
+++ beantowncompost/managers/signals.py
@@ -1,9 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import ManagerProfile
 from django.core.exceptions import ObjectDoesNotExist
 
+"""
 @receiver(post_save, sender=User)
 def create_managerprofile(sender, instance, created, **kwargs):
     if created:
@@ -16,3 +16,4 @@ def save_managerprofile(sender, instance, **kwargs):
     except ObjectDoesNotExist:
         ManagerProfile.objects.create(user=instance)
 
+"""
