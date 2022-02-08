b78daa2a34fdb636eb1b126212917acf3e73da16
Tal Zaken <talzaken@gmail.com>
2022-02-07 22:24:05 -0500

    urls for suggesting locations and reviewing them

--- beantowncompost/dropoff_locations/urls.py
+++ beantowncompost/dropoff_locations/urls.py
@@ -5,10 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('vote/', views.vote),
-    path('add_location/', views.add_location),
+    path('add_location/', views.add_location, name='add_location'),
+    path('suggest_location/', views.suggest_location, name='suggest_location'),
     path('correct_location/', views.correct_location),
     path('update_location/', views.update_location),
     path('update_site_managers/', views.update_site_managers, name='update_site_managers'),
     path('request_management_permission/', views.request_management_permission, name='request_management_permission'),
+    path('review_suggested_locations/', views.review_suggested_locations, name='review_suggested_locations'),
     path('locations/', views.locations),
 ]
