05f827ba743aae099e9e95e58778876f5ee223ca
Tal Zaken <talzaken@gmail.com>
2022-02-07 22:31:04 -0500

    removed admin model

--- beantowncompost/managers/admin.py
+++ beantowncompost/managers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ManagerProfile, ManagerSitePermission
+from .models import ManagerSitePermission
 # Register your models here.
 
-admin.site.register(ManagerProfile, admin.ModelAdmin)
+#admin.site.register(ManagerProfile, admin.ModelAdmin)
 admin.site.register(ManagerSitePermission, admin.ModelAdmin)
